6e9071bb37db6ce2cf875262d4d7b253c9e670d0
Daisensei(Win) <apb5377@gmail.com>
2022-10-05 02:05:55 +0900

    Fixed ToTextData method to correctly output 2 bytes.

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 import java.io.*;
 
 public class Main {
-    public static final String PICTURE_NAME = "4.png";
-    public static final String TEXT_FILE_NAME = "testFile.txt";
+    public static final String PICTURE_NAME = "アレキサンダー.png";
+    //public static final String PICTURE_NAME = "8440.png";
+    public static final String TEXT_FILE_NAME = "アレキサンダー.txt";
     public static void main(String[] args){
         /*
         if(args.length > 2){
@@ -12,8 +13,9 @@ public class Main {
             System.out.println("引数が足りません");
          */
 
-        ToTextData(PICTURE_NAME,TEXT_FILE_NAME);
+        //ToTextData(PICTURE_NAME,TEXT_FILE_NAME);
         //ToTextData("testFile.bin","testFile.txt");
+        ToBinaryData(PICTURE_NAME,TEXT_FILE_NAME);
     }
 
     private static void ToTextData(String binaryFile,String textFile){
@@ -23,6 +25,8 @@ public class Main {
             String tmp;
             while(inputStream.available() > 0) {
                 tmp = Integer.toHexString(inputStream.read());
+                if(tmp.length() == 1)
+                    tmp = "0" + tmp;
                 System.out.print(tmp);
                 for(char c : tmp.toCharArray()) {
                     outputStream.write(c);
@@ -30,7 +34,29 @@ public class Main {
             }
             inputStream.close();
         }catch(Exception e){
-            System.out.println(e);
+            System.out.println(e.toString());
+        }
+    }
+
+    private static void ToBinaryData(String binaryFile,String textFile){
+        try{
+            InputStream inputStream = new FileInputStream(textFile);
+            OutputStream outputStream = new FileOutputStream(binaryFile);
+
+            String tmp;
+            char[] c = new char[2];
+            byte b;
+            while(inputStream.available() > 0){
+                c[0] = (char)inputStream.read();
+                c[1] = (char)inputStream.read();
+
+                tmp = new String(c);
+                b = (byte)Integer.parseInt(tmp,16);
+                System.out.println(tmp);
+                outputStream.write(b);
+            }
+        }catch(Exception e){
+            System.out.println(e.toString());
         }
     }
 }
